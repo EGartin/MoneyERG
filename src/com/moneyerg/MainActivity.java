ab4f9db30745faa5995ca70ea9512347176ce0c7
Devin Brown <devin.g.brown@gmail.com>
2013-03-23 11:42:03 -0600

    LOLOLOLOOLOOLOLOLOO

--- src/com/moneyerg/MainActivity.java
+++ src/com/moneyerg/MainActivity.java
@@ -9,60 +9,59 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private Button button;
-    private Editable hoursWorked;
-    private Editable wageRate;
-    private double overtimeRate;
-    private double result;
-	private double wageRateNum;
-	private double hoursWorkedNum;
+  private Button button;
+  private Editable hoursWorked;
+  private Editable wageRate;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {    	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        
-        EditText text = (EditText) findViewById(R.id.hoursWorked);
-        hoursWorked = text.getText();
-        hoursWorkedNum = Double.parseDouble(hoursWorked.toString());
-        
-        EditText text1 = (EditText) findViewById(R.id.wageAmount);
-        wageRate = text1.getText();
-        wageRateNum = Double.parseDouble(wageRate.toString());
-        overtimeRate = wageRateNum*1.5;
+    EditText text = (EditText) findViewById(R.id.hoursWorked);
+    hoursWorked = text.getText();
 
-        button = (Button) findViewById(R.id.calculateButton);
+    EditText text1 = (EditText) findViewById(R.id.wageAmount);
+    wageRate = text1.getText();
 
-        button.setOnClickListener(new OnClickListener() {
+    button = (Button) findViewById(R.id.calculateButton);
 
-            @Override
-            public void onClick(View arg0) {
+    button.setOnClickListener(new OnClickListener() {
 
-            	if(hoursWorkedNum <= 40){
-            		result = hoursWorkedNum * wageRateNum;
-            	}
-            	else if (hoursWorkedNum > 40){
-            		result = hoursWorkedNum * overtimeRate;	
-            	}
-                // Toast.makeText(getApplicationContext(), "$$$: " + result,
-                // Toast.LENGTH_LONG).show();
+      @Override
+      public void onClick(View arg0) {
 
-                TextView text = (TextView) findViewById(R.id.totalWages);
-                text.setText(Double.toString(result));
+        // double-ize the input Editable fields
+        double hw = Double.valueOf(hoursWorked.toString());
+        double wr = Double.valueOf(wageRate.toString());
 
-            }
-        });
-    }
+        double result = 0;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+        if (hw <= 40) {
+          result = hw * wr;
+        } else {
+          // first 40 hours @ normal wage + remaining OT hours at time & half
+          result = (hw * wr) + ((hw - 40) * (wr * 1.5));
+        }
 
+        // Toast.makeText(getApplicationContext(), "REGULAR TIME: " + result,
+        // Toast.LENGTH_LONG).show();
+
+        TextView text = (TextView) findViewById(R.id.totalWages);
+        text.setText(Double.toString(result));
+
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
 }
